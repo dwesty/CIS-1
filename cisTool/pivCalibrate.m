7869f2be2067e6f5807ae8e7ae71d9191bfb991c
David West <drwest15@gmail.com>
2015-10-10 16:24:40 -0700

    modifying pivot calibration...about to start testing (parameter estimataion?)

--- cisTool/pivCalibrate.m
+++ cisTool/pivCalibrate.m
@@ -1,12 +1,12 @@
 % Pivot calibration
 % Assume some know vector F from tracker to marker
-% R is rotational component of F
-% p is translational component of F
-% Solving for p_tip, the vector from marker to tip
+% R_i is rotational component of F (vector entry for each i)
+% p_i is translational component of F (vector entry for each i)
+% Solving for p_t, p_pivot, the vector from marker to tip
 % Pivot calibration assumes this is fixed
-function p_tip = pivCalibrate(R, p)
+function p_t = pivCalibrate(R_i, p_i)
 
-% in progress... DW
-p_tip = inv(R)*(p-t)
+% in progress... DW[
+[p_t p_pivot] = pinv(R_i)*(p_i-t)
 
 end
