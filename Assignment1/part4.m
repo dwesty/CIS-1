13c6058f8e4c86cdaa1ec41f82dcad0e09410e67
David West <drwest15@gmail.com>
2015-10-11 13:55:59 -0700

    moved some files, copied necessary files to Assignment1, working on part4. debugging least squares

---
+++ Assignment1/part4.m
@@ -0,0 +1,26 @@
+%% Number 4
+% A - Computing frame
+clear
+
+calBody = fopen('pa1-debug-a-calbody.txt')
+
+infoLine = fgetl(calBody);
+scanner = textscan(infoLine, '%f%f%f%s', 'delimiter', ',');
+numBaseOpMarkers = scanner{1,1}
+numOpMarkers = scanner{1,2}
+numEmMarkers = scanner{1,3}
+
+baseOpMarkers = parseFile(calBody, numBaseOpMarkers)
+
+calReadings = fopen('pa1-debug-a-calreadings.txt')
+
+infoLine2 = fgetl(calReadings);
+scanner2 = textscan(infoLine2, '%f%f%f%s', 'delimiter', ',');
+numBaseOpReadings = scanner2{1,1}
+numOpReadings = scanner2{1,2}
+numEmReadings = scanner2{1,3}
+
+baseOpReadings = parseFile(calReadings, numBaseOpReadings)
+F_d = lsqnonneg(baseOpReadings, baseOpMarkers)
+
+fclose('all')
