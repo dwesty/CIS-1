8cb9dc8b40785b0f1f124162032b29a83efe3244
Kevin Yee <yeekevin11@gmail.com>
2015-10-11 21:47:09 -0400

    Use part2_function to calculate transformation for part 4a

--- Assignment1/part4.m
+++ Assignment1/part4.m
@@ -1,30 +1,28 @@
 % Number 4
 % A - Computing frame
-clear
+clear;
 
-calBody = fopen('pa1-debug-a-calbody.txt')
+calBody = fopen('pa1-debug-a-calbody.txt');
 
 infoLine = fgetl(calBody);
 scanner = textscan(infoLine, '%f%f%f%s', 'delimiter', ',');
-numBaseOpMarkers = scanner{1,1}
-numOpMarkers = scanner{1,2}
-numEmMarkers = scanner{1,3}
+numBaseOpMarkers = scanner{1,1};
+numOpMarkers = scanner{1,2};
+numEmMarkers = scanner{1,3};
 
 baseOpMarkers = parseFile(calBody, numBaseOpMarkers)
 
-calReadings = fopen('pa1-debug-a-calreadings.txt')
+calReadings = fopen('pa1-debug-a-calreadings.txt');
 
 infoLine2 = fgetl(calReadings);
 scanner2 = textscan(infoLine2, '%f%f%f%s', 'delimiter', ',');
-numBaseOpReadings = scanner2{1,1}
-numOpReadings = scanner2{1,2}
-numEmReadings = scanner2{1,3}
+numBaseOpReadings = scanner2{1,1};
+numOpReadings = scanner2{1,2};
+numEmReadings = scanner2{1,3};
 
 baseOpReadings = parseFile(calReadings, numBaseOpReadings)
 
-%Should not be getting 0
-% F_d = [ones(8,1),baseOpMarkers]\baseOpReadings;
 
-[regParams,Bfit,ErrorStats] = absor(baseOpMarkers',baseOpReadings') 
+[R p] = part2_function(baseOpMarkers,baseOpReadings)
 
-fclose('all')
+fclose('all');
