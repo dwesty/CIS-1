32d855aa59887f52610bedfe1f750ea6775c1a71
David West <drwest15@gmail.com>
2015-10-12 17:33:47 -0700

    fixing 3

--- Assignment1/part3.m
+++ Assignment1/part3.m
@@ -7,20 +7,21 @@
 
 clear
 
-path = 'C:\Users\Kevin\Desktop\CIS-1\Assignment1\PA-12 Student Data\';
+% path = 'C:\Users\Kevin\Desktop\CIS-1\Assignment1\PA-12 Student Data\';
 fileName = 'pa1-unknown-h-empivot.txt';
 
-emPivot = fopen(fullfile(path,fileName));
+% emPivot = fopen(fullfile(path,fileName))
+emPivot = fopen(fileName)
 
 infoLine = fgetl(emPivot);
 scanner = textscan(infoLine, '%f%f%s', 'delimiter', ',');
 numEmMarkers = scanner{1,1};
 numFrames = scanner{1,2};
 
+G = parseFile(emPivot,numEmMarkers);
+G_piv = parseFile(emPivot,numFrames);
 
 for i=1:numFrames
-    
-    G = parseFile(emPivot,numEmMarkers);
     
     sumG = [0,0,0];
     for j=1:numEmMarkers
@@ -36,8 +37,10 @@ for i=1:numFrames
 
     %FIXME: Currently returns R=I and p=centroidG.
     %I don't think that is correct
-    [Fg_R,Fg_p] = part2_function(g_j,G)
+    [Fg_R,Fg_p] = part2_function(g_j,G_piv);
 end
+Fg_R
+Fg_p
 
 fclose('all');
 
