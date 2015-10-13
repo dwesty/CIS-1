0989bb32d76b486b3705fa629120b3e2242fdeca
Kevin Yee <yeekevin11@gmail.com>
2015-10-13 10:47:52 -0400

    Convert partX.m to functions with file names as input parameters

---
+++ Assignment1/computeCexp.m
@@ -0,0 +1,81 @@
+%{
+    CIS Programming Assignment 1
+    Part 4
+
+    Kevin Yee and David West
+    10/13/2015
+%}
+
+function [C_exp] = computeCexp(fullBodyFile,fullReadingsFile)
+% Input parameters must be full file name including path
+
+% Get d,a and c from calbody
+calBody = fopen(fullBodyFile);
+infoLine = fgetl(calBody);
+scanner = textscan(infoLine, '%f%f%f%s', 'delimiter', ',');
+numBaseOpMarkers = scanner{1,1};
+numOpMarkers = scanner{1,2};
+numEmMarkers = scanner{1,3};
+baseOpMarkers = parseFile(calBody, numBaseOpMarkers);
+OpMarkers = parseFile(calBody, numOpMarkers);
+emMarkers = parseFile(calBody, numEmMarkers);
+
+% Get D, A and C from calreadings
+calReadings = fopen(fullReadingsFile);
+infoLine2 = fgetl(calReadings);
+scanner2 = textscan(infoLine2, '%f%f%f%f%s', 'delimiter', ',');
+numBaseOpReadings = scanner2{1,1};
+numOpReadings = scanner2{1,2};
+numEmReadings = scanner2{1,3};
+numFrames = scanner2{1,4};
+
+entireD = [];
+entireA = [];
+entireC = [];
+for i=1:numFrames
+    entireD = [entireD;parseFile(calReadings,numBaseOpReadings)];
+    entireA = [entireA;parseFile(calReadings,numOpReadings)];
+    entireC = [entireC;parseFile(calReadings,numEmReadings)];
+end
+
+fclose('all');
+
+aTrans = zeros(numEmMarkers,3);
+C_exp = zeros(numEmMarkers*numFrames,3);
+for i=1:numFrames
+    
+    % Get the current D frame from entireD
+    currFrameD = zeros(numBaseOpMarkers,3);
+    for j=1:numBaseOpMarkers
+        currFrameD(j,:) = entireD((i-1)*numBaseOpMarkers+j,:);
+    end
+    
+    % Get the current A frame from entireA
+    currFrameA = zeros(numOpMarkers,3);
+    for j=1:numOpMarkers
+        currFrameA(j,:) = entireA((i-1)*numBaseOpMarkers+j,:);
+    end
+
+    % Calculate F_d using part 2
+    [R_d,p_d] = part2_function(baseOpMarkers,currFrameD);
+
+    % Calculate the inverse of F_d
+    [invFd_R,invFd_p] = invTransformation(R_d,p_d);
+
+    % Calculate F_a using part 2
+    [R_a,p_a] = part2_function(OpMarkers,currFrameA);
+    
+    % Calculate next available spot in C_est
+    index = (i-1)*numEmMarkers;
+        
+    % Perform transformations to find C_est
+    for j=1:numEmMarkers
+        aTrans(j,:) = R_a*emMarkers(j,:)' + p_a;
+        C_exp(index+j,:) = invFd_R*aTrans(j,:)' + invFd_p;
+    end
+    
+end
+ 
+C_exp
+end
+
---
+++ Assignment1/emPivotCalibration.m
@@ -0,0 +1,69 @@
+%{
+    CIS Programming Assignment 1
+    Part 5: EM Pivot Calibration
+
+    Kevin Yee and David West
+%}
+
+function [t_G p_dimple] = emPivotCalibration(fullEmPivotFileName)
+% Input parameter must be full file name including file path
+
+
+% Open file and parse first line of information
+emPivot = fopen(fullEmPivotFileName);
+infoLine = fgetl(emPivot);
+scanner = textscan(infoLine, '%f%f%s', 'delimiter', ',');
+numEmMarkers = scanner{1,1};
+numFrames = scanner{1,2};
+
+% Get first frame data from file
+G = parseFile(emPivot,numEmMarkers);
+
+% Calculate center of point set by averaging all points
+sumG = [0,0,0];
+for j=1:numEmMarkers
+    sumG = sumG + G(j,:);
+end
+centroidG = sumG/numEmMarkers;
+
+% Center each point to the center of the set
+g_j = 0*G;
+for j=1:numEmMarkers
+    g_j(j,:) = G(j,:) - centroidG;
+end
+
+A = zeros(3*(numFrames-1),6);
+b = zeros(3*(numFrames-1),1);
+for i=1:(numFrames-1)
+    
+    % Get frame data from file
+    G = parseFile(emPivot,numEmMarkers);
+    
+    % Calculate Fg with part 2
+    [Fg_R,Fg_p] = part2_function(g_j,G);
+        
+    % Create data matrix for least squares
+    index = 3*i-2;
+    A(index  ,:) = [Fg_R(1,:),-1,0,0];
+    A(index+1,:) = [Fg_R(2,:),0,-1,0];
+    A(index+2,:) = [Fg_R(3,:),0,0,-1];
+    
+    % Create vector of translations for least squares
+    b(index  ) = Fg_p(1);
+    b(index+1) = Fg_p(2);
+    b(index+2) = Fg_p(3);
+   
+end
+
+% Solve Ax = b by least squares
+x = A\(-b);
+
+% Extract relative tip location and post location from x
+t_G = [x(1);x(2);x(3)]
+p_dimple = [x(4);x(5);x(6)]
+
+fclose('all');
+
+
+end
+
