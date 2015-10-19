ccf57ea8a45b04ca9febfb9f99d211920cdd87ee
David West <drwest15@gmail.com>
2015-10-18 22:35:24 -0400

    working on 3....went 3d on 2

--- Assignment2/correctedEMPivot.m
+++ Assignment2/correctedEMPivot.m
@@ -1,16 +1,7 @@
-%{
-    CIS Programming Assignment 1
-    Part 5: EM Pivot Calibration
+%assignment1Part3
 
-    Kevin Yee and David West
-%}
+filename = 'pa1-debug-a-empivot.txt';
 
-function [t_G p_dimple] = emPivotCalibration(emPivotFileName)
-% Input parameter must be file name only (not including file path)
-
-
-% Open file and parse first line of information
-filename = ['PA-12 Student Data/',emPivotFileName];
 emPivot = fopen(filename);
 infoLine = fgetl(emPivot);
 scanner = textscan(infoLine, '%f%f%s', 'delimiter', ',');
@@ -20,17 +11,25 @@ numFrames = scanner{1,2};
 % Get first frame data from file
 G = parseFile(emPivot,numEmMarkers);
 
+
+gMin = min(g);
+gMax = max(g);
+
+% Correct for distortion
+G_corrected_scaled = correctDistortion(bezierCoeff, G);
+G_corrected = unscaleFromBox(G_corrected_scaled,gMax,gMin)
+
 % Calculate center of point set by averaging all points
 sumG = [0,0,0];
 for j=1:numEmMarkers
-    sumG = sumG + G(j,:);
+    sumG = sumG + G_corrected(j,:);
 end
 centroidG = sumG/numEmMarkers;
 
 % Center each point to the center of the set
-g_j = 0*G;
+g_j = 0*G_corrected;
 for j=1:numEmMarkers
-    g_j(j,:) = G(j,:) - centroidG;
+    g_j(j,:) = G_corrected(j,:) - centroidG;
 end
 
 A = zeros(3*(numFrames-1),6);
@@ -38,10 +37,10 @@ b = zeros(3*(numFrames-1),1);
 for i=1:(numFrames-1)
     
     % Get frame data from file
-    G = parseFile(emPivot,numEmMarkers);
+    G_corrected = parseFile(emPivot,numEmMarkers);
     
     % Calculate Fg with part 2
-    [Fg_R,Fg_p] = part2_function(g_j,G);
+    [Fg_R,Fg_p] = part2_function(g_j,G_corrected);
         
     % Create data matrix for least squares
     index = 3*i-2;
@@ -64,7 +63,3 @@ t_G = [x(1);x(2);x(3)]
 p_dimple = [x(4);x(5);x(6)]
 
 fclose('all');
-
-
-end
-
