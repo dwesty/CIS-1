76dbd96a50b0534995d6eed71fbec253e292b7f6
Kevin Yee <yeekevin11@gmail.com>
2015-11-06 14:19:00 -0500

    Reading input data for PA3. Fix reading the .sur file

---
+++ Assignment3/getCoordinates.m
@@ -0,0 +1,10 @@
+function coordinates = getCoordinates(file,numCoords)
+    coordinates = zeros(3,numCoords);
+    for i = 1:numCoords
+        scanner = textscan(fgetl(file),'%f%f%f','delimiter',',');
+        for j = 1:3
+            coordinates(j,i) = scanner{1,j};
+        end
+    end
+end
+
---
+++ Assignment3/main_pa3.m
@@ -0,0 +1,64 @@
+%{
+    Programming Assignment 3
+    Main Script
+
+    Kevin Yee & David West
+    11/06/2015
+    Computer Integrated Surgery I
+%}
+clear;
+
+% Input File Path
+inputFilePath = '../PA-345 Student Data/';
+
+% Rigid Body Design File A
+problemFilePathA = [inputFilePath,'Problem3-BodyA.txt'];
+problemFileA = fopen(problemFilePathA);
+aScanner = textscan(fgetl(problemFileA),'%f%s','delimiter',',');
+numMarkersA = aScanner{1,1};
+markersA = getCoordinates(problemFileA,numMarkersA);
+tipA = getCoordinates(problemFileA,1);
+
+% Rigid Body Design File B
+problemFilePathB = [inputFilePath,'Problem3-BodyB.txt'];
+problemFileB = fopen(problemFilePathB);
+bScanner = textscan(fgetl(problemFileB),'%f%s','delimiter',',');
+numMarkersB = bScanner{1,1};
+markersB = getCoordinates(problemFileB,numMarkersB);
+tipB = getCoordinates(problemFileB,1);
+
+% Mesh File
+meshFilePath = 'Problem3Mesh.txt';
+%meshFilePath = [inputFilePath,'Problem3Mesh.sur'];%FIXME: Read in sur file
+meshFile = fopen(meshFilePath);
+
+meshScanner = textscan(fgetl(meshFile),'%f');
+numVertices = meshScanner{1,1};
+vertices = getCoordinates(meshFile,numVertices);
+
+% Get triangle adjacency indices
+meshScanner = textscan(fgetl(meshFile),'%f');
+numTriangles = meshScanner{1,1};
+adjacencies = getTriangles(meshFile,numTriangles);
+
+% Sample Readings File
+run = 'A-Debug';
+sampleFilePath = [inputFilePath,'PA3-',run,'-SampleReadingsTest.txt'];
+sampleFile = fopen(sampleFilePath);
+sampleScanner = textscan(fgetl(sampleFile),'%f%f%s','delimiter',',');
+numTotalLeds = sampleScanner{1,1};
+numSamples = sampleScanner{1,2};
+
+% Dummy Markers are all remaining after A and B
+numDummy = numTotalLeds - numMarkersA - numMarkersB;
+
+aMarkersTracker = zeros(3,numMarkersA,numSamples);
+bMarkersTracker = zeros(3,numMarkersB,numSamples);
+dummyMarkersTracker = zeros(3,numDummy,numSamples);
+for i = 1:numSamples
+    aMarkersTracker(:,:,i) = getCoordinates(sampleFile,numMarkersA);
+    bMarkersTracker(:,:,i) = getCoordinates(sampleFile,numMarkersB);
+    dummyMarkersTracker(:,:,i) = getCoordinates(sampleFile,numDummy);
+end
+
+fclose('all');
