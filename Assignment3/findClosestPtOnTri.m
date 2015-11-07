4d55e1fef96f4fbf8582c8294038976f05d87924
Kevin Yee <yeekevin11@gmail.com>
2015-11-07 12:56:33 -0500

    Convert projected point to barycentric coordinates

--- Assignment3/findClosestPtOnTri.m
+++ Assignment3/findClosestPtOnTri.m
@@ -3,25 +3,18 @@ function closestPt = findClosestPtOnTri(pt,tri)
 % triangle: the three column vector points that makeup
 %           the vertices of the triangle
 
-twoMinusOne = tri(:,2) - tri(:,1)
-threeMinusOne = tri(:,3) - tri(:,1)
+twoMinusOne = tri(:,2) - tri(:,1);
+threeMinusOne = tri(:,3) - tri(:,1);
 
 % Calculate unit normal vector
 normal = cross(twoMinusOne,threeMinusOne);
 unitNormal = normal/norm(normal);
 projection = pt - dot(pt - tri(:,1),unitNormal)*unitNormal;
 
+T = [1,2,3];
+TR = triangulation(T,tri');
 
-% Make plane into two dimensions (z = 0)
-% Pt 1: Origin, Pt 2: [norm(twoMinusOne);0;0], Pt 3: corresponding point
-angle = subspace(threeMinusOne,twoMinusOne);
-pt1 = [0;0;0]
-pt2 = [norm(twoMinusOne);0;0]
-pt3 = [cos(angle)*norm(threeMinusOne);sin(angle)*norm(threeMinusOne);0]
-
-
-
-closestPt = projection;
+B = cartesianToBarycentric(TR,1,projection')
 
 end
 
